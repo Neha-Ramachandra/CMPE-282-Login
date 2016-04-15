c141d20b97c29c112ab2b02ae1fa65c9dd7b7056
Mahek Pavagadhi <Mahekp@users.noreply.github.com>
2016-04-15 12:46:58 -0700

    Update EmployeeDeletionServlet.java

--- src/main/java/com/enterprisedatabase/servlets/EmployeeDeletionServlet.java
+++ src/main/java/com/enterprisedatabase/servlets/EmployeeDeletionServlet.java
@@ -23,19 +23,38 @@ public class EmployeeDeletionServlet extends HttpServlet {
     }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		String employeeId = request.getParameter("employeenum");
+		PrintWriter printWriter= response.getWriter();
+		try
+		{
+		boolean isValid = employeeDAO.isIdValidEmployee(Integer.parseInt(employeeId));
+			if(isValid)
+			{
+				employeeDAO.deleteAEmployee(Integer.parseInt(employeeId));
+				String htmlRespone = "<html>";
+				htmlRespone += "<h2>Your order is successfully deleted with employeeId=: " + employeeId + "</h2>";
+				htmlRespone += "</html>";
+				printWriter.println(htmlRespone);
+			}
+			else
+			{
+				String htmlRespone1 = "<html>";
+				htmlRespone1 += "<h2><b>There are no employees under the</b> </br> employeeId=: " + employeeId + "</h2>";
+				htmlRespone1 += "</html>";
+				printWriter.println(htmlRespone1);
+			}	
+		}
+	
+		catch(Exception e)
+		{
+			String htmlRespone = "<html>";
+			htmlRespone += "<h2>The employee details with employeeId: " + employeeId + " cannot be deleted.</h2>";
+			htmlRespone += "</html>";
 
+			printWriter.println(htmlRespone);
+		}	      
+	}
 }
