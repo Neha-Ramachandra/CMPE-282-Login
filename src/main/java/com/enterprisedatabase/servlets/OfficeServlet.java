5a2f748f384316cd63e8e6fbf149e6abdcb594e3
Mahek Pavagadhi <Mahekp@users.noreply.github.com>
2016-04-16 11:58:48 -0700

    Update OfficeServlet.java

--- src/main/java/com/enterprisedatabase/servlets/OfficeServlet.java
+++ src/main/java/com/enterprisedatabase/servlets/OfficeServlet.java
@@ -22,17 +22,14 @@ public class OfficeServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		List<Office> officedetails = officesDAO.findAllOffices();
+			request.setAttribute("officedetails", officedetails);
+			request.getRequestDispatcher("/WEB-INF/JSPs/officedetails.jsp").forward(request, response);
 	}
+		
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
