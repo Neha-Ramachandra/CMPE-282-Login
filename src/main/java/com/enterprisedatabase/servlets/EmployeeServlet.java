15fba5c8e3befa0c10c81cae95caf71183bd6d88
Mahek Pavagadhi <Mahekp@users.noreply.github.com>
2016-04-16 20:34:04 -0700

    Update EmployeeServlet.java

--- src/main/java/com/enterprisedatabase/servlets/EmployeeServlet.java
+++ src/main/java/com/enterprisedatabase/servlets/EmployeeServlet.java
@@ -22,21 +22,47 @@ public class EmployeeServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		List<Employee> employeedetails = employeeDAO.findAllEmployees();
 			request.setAttribute("employeedetails", employeedetails);
-			request.getRequestDispatcher("/WEB-INF/JSPs/employeedetails.jsp").forward(request, response);
+			request.getRequestDispatcher("/WEB-INF/JSPs/allemployeeslist.jsp").forward(request, response);
 		}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		String employeeName = request.getParameter("FirstName");
+		String officeCode = request.getParameter("officeCode");
+		PrintWriter printWriter= response.getWriter();   
+		try
+		{		
+			EmployeeDAO employeeDAO = new EmployeeDAO();
+		    boolean isValid = employeeDAO.isIdValidEmployee(Integer.parseInt(officeCode));
+		    if(isValid)
+			employeeDAO.updateemployeeName(Integer.parseInt(officeCode), employeeName);
+		    	String htmlRespone = "<html>";
+			    htmlRespone += "</html>";		     
+			    printWriter.println(htmlRespone);
+				List<Employee> employeedetails = employeeDAO.findEmployee(Integer.parseInt(officeCode));
+					request.setAttribute("employeedetails", employeedetails);
+					request.getRequestDispatcher("/WEB-INF/JSPs/allemployeeslist.jsp").forward(request, response);
+		}
+		    else
+		      {
+			String htmlRespone1 = "<html>";
+			    htmlRespone1 += "<h2><b>There are no employee under the</b> </br> officeCode=: " + officeCode + "</h2>";
+			    htmlRespone1 += "</html>";
+			    printWriter.println(htmlRespone1);
+			}
+		}
+		catch(Exception e)
+		{
+			String htmlRespone = "<html>";
+		    htmlRespone += "<h2>There are no employees with officeCode=: " + officeCode + "</h2>";
+		    htmlRespone += "</html>";
+		     
+		    printWriter.println(htmlRespone);
+		}	 
 	}
-
+	
 }
