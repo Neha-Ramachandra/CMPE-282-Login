6d99c526215e3da63c52747d20f8e3546afc33a1
nikita28749 <nikita.mathur@sjsu.edu>
2016-04-19 20:58:48 -0700

    Update PaymentServlet.java

--- src/main/java/com/enterprisedatabase/servlets/PaymentServlet.java
+++ src/main/java/com/enterprisedatabase/servlets/PaymentServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/payments")
 public class PaymentServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	PaymentDAO paymentDAO = new PaymentDAO();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -26,16 +27,54 @@ public class PaymentServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		List<Payment> paymentdetails = paymentDAO.listAllPayment();
+		request.setAttribute("paymentdetails", paymentdetails);
+		request.getRequestDispatcher("/WEB-INF/JSPs/paymentlist.jsp").forward(request,response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	    String customerId = request.getParameter("customernum");
+	    PrintWriter printWriter = response.getWriter();
+	    
+	    
+	   try
+	   
+	   { 
+	   	boolean isValid = paymentDAO.isIDValidCustomer(Interger.parseInt(customerId));
+	   	if (isValid)
+	   	
+	   	{ 
+	   		List<Payment> paymentinfo = PaymentDAO.findPaymentDetailsOfACustomer(Integer.parseInt(customerId));
+	   		request.setAttribute("paymentinfo",paymentinfo);
+	   		request.getRequestDispatcher("/WEB-INF/JSPs/paymentdetailsofcustomerbyid.jsp").forward(request,response);
+	   	}
+	   	
+	   	else
+	   	
+	   	{
+	   		    String htmlRespone1 = "<html>"; 
+			    htmlRespone1 += "<h2><b>There are no customers under the</b> </br> customerId=: " + customerId + "</h2>"; 
+ 			    htmlRespone1 += "</html>"; 
+ 			    printWriter.println(htmlRespone1); 
+
+	   	}
+	   }
+	   
+	   catch(Exception e) 
+		{ 
+ 	            String htmlRespone = "<html>"; 
+ 		    htmlRespone += "<h2>There are no payment details available for the customer with customerId=: " + customerId + "</h2>"; 
+ 		    htmlRespone += "</html>"; 
+ 		      
+ 		    printWriter.println(htmlRespone); 
+ 		} 
+
+	   
+	   
+	            }
 	}
 
-}
+
